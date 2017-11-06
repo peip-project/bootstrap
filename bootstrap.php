d45de742b12eaf9d6443dbf5b667e86ec2bd6f77
Timo Michna <timomichna@yahoo.de>
2017-11-06 14:10:30 +0100

    use Bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -1,31 +1,14 @@
 <?php
 
+namespace PEIP;
+
 /**
  * Find the auto loader file
  */
-$local = __DIR__.'/autoload.php';
-if(is_file($local)) {
-    require_once $local;
-    return;
-}
 
-$pharPath = \Phar::running(true);
-if ($pharPath && is_file("$pharPath/vendor/autoload.php")) {
-    require_once "$pharPath/vendor/autoload.php";
-} else {
-    $depth = 5;
-    for ($x = 0; $x <= $depth; $x++) {
-        $prefix = str_repeat('../', $x);
-        $autoload = __DIR__ . '/'.$prefix.'vendor/autoload.php';
-        if (file_exists($autoload)) {
-            require_once $autoload;
-            return;
-        }
-    }
+ require_once __DIR__.'/src/Bootstrap.php';
 
-    throw new \RuntimeException('Could not resolve autoload file');
-}
-
+ Bootstrap::run();
 
 
 
